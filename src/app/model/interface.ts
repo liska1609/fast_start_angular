50c78d7822e3f5f74b6c1b7d3d2a770b75e71ac9
Vasilissa <nancy_94@mail.ru>
2024-02-03 20:09:23 +0500

    БС Angular 01. Введение

---
+++ src/app/model/interface.ts
@@ -0,0 +1,13 @@
+export interface Course {
+    id: number,
+    title: string,
+    creation_date: Date,
+    duration: Date,
+    description: string
+}
+
+export interface User {
+    id: number,
+    first_name: string,
+    last_name: string
+}
